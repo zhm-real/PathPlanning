b25bfec8d3a3b44984cfb171ab147add1da6b725
yue qi <391311qy@gmail.com>
2020-06-26 23:05:10 -0700

    A*changed

---
+++ Search-based Planning/Astar_3D/Astar3D.py
@@ -0,0 +1,38 @@
+# this is the three dimensional A* algo
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: yue qi
+"""
+import numpy as np
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../Search-based Planning/")
+from Astar_3D.env3D import env
+from Astar_3D.utils3D import getAABB, getDist, getRay, StateSpace, Heuristic
+from queue import QueuePrior
+
+
+class Weighted_A_star(object):
+    def __init__(self):
+        self.Alldirec = np.array([[1 ,0,0],[0,1 ,0],[0,0, 1],[1 ,1 ,0],[1 ,0,1 ],[0, 1, 1],[ 1, 1, 1],\
+                      [-1,0,0],[0,-1,0],[0,0,-1],[-1,-1,0],[-1,0,-1],[0,-1,-1],[-1,-1,-1],\
+                      [1,-1,0],[-1,1,0],[1,0,-1],[-1,0, 1],[0,1, -1],[0, -1,1],\
+                      [1,-1,-1],[-1,1,-1],[-1,-1,1],[1,1,-1],[1,-1,1],[-1,1,1]])
+        self.env = env()
+        self.Space = StateSpace(env.boundary) # key is the point, store g value
+        self.OPEN = QueuePrior() # store [point,priority]
+        self.start = getNearest(self.Space,env.start)
+        self.goal = getNearest(self.Space,env.goal)
+        self.h = Heuristic(self.Space,self.goal)
+        self.Parent = {}
+        self.CLOSED = {}
+        
+
+    def run(self):
+        pass
+if __name__ == '__main__':
+    Astar = Weighted_A_star()
+    

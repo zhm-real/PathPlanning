676e267964e53bb7dee9112f3a38a484135111d0
zhm-real <zhou.hm0420@gmail.com>
2020-07-09 16:27:00 -0700

    modify Dstar

--- Search-based Planning/Search_3D/Dstar3D.py
+++ Search-based Planning/Search_3D/Dstar3D.py
@@ -8,25 +8,26 @@ from collections import defaultdict
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../Search-based Planning/")
 from Search_3D.env3D import env
 from Search_3D import Astar3D
-from Search_3D.utils3D import StateSpace, getDist, getNearest, getRay, isinbound, isinball, isCollide, children, cost, initcost
+from Search_3D.utils3D import StateSpace, getDist, getNearest, getRay, isinbound, isinball, isCollide, children, cost, \
+    initcost
 from Search_3D.plot_util3D import visualization
 
 
 class D_star(object):
-    def __init__(self,resolution = 1):
+    def __init__(self, resolution=1):
         self.Alldirec = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 0], [1, 0, 1], [0, 1, 1], [1, 1, 1],
                                   [-1, 0, 0], [0, -1, 0], [0, 0, -1], [-1, -1, 0], [-1, 0, -1], [0, -1, -1],
                                   [-1, -1, -1],
                                   [1, -1, 0], [-1, 1, 0], [1, 0, -1], [-1, 0, 1], [0, 1, -1], [0, -1, 1],
                                   [1, -1, -1], [-1, 1, -1], [-1, -1, 1], [1, 1, -1], [1, -1, 1], [-1, 1, 1]])
-        self.env = env(resolution = resolution)
+        self.env = env(resolution=resolution)
         self.X = StateSpace(self.env)
         self.x0, self.xt = getNearest(self.X, self.env.start), getNearest(self.X, self.env.goal)
-        self.b = defaultdict(lambda: defaultdict(dict))# back pointers every state has one except xt.
-        self.OPEN = {} # OPEN list, here use a hashmap implementation. hash is point, key is value 
-        self.h = self.initH() # estimate from a point to the end point
-        self.tag = self.initTag() # set all states to new
-        self.V = set()# vertice in closed
+        self.b = defaultdict(lambda: defaultdict(dict))  # back pointers every state has one except xt.
+        self.OPEN = {}  # OPEN list, here use a hashmap implementation. hash is point, key is value
+        self.h = self.initH()  # estimate from a point to the end point
+        self.tag = self.initTag()  # set all states to new
+        self.V = set()  # vertice in closed
         # initialize cost set
         # self.c = initcost(self)
         # for visualization
@@ -35,7 +36,6 @@ class D_star(object):
         self.done = False
         self.Obstaclemap = {}
 
-        
     def update_obs(self):
         for xi in self.X:
             print('xi')
@@ -57,7 +57,7 @@ class D_star(object):
         # tag , New point (never been in the OPEN list)
         #       Open point ( currently in OPEN )
         #       Closed (currently in CLOSED)
-        t = {} 
+        t = {}
         for xi in self.X:
             t[xi] = 'New'
         return t
@@ -66,10 +66,7 @@ class D_star(object):
         # get the minimum of the k val in OPEN
         # -1 if it does not exist
         if self.OPEN:
-            minv = np.inf
-            for v,k in enumerate(self.OPEN):
-                if v < minv: minv = v
-            return minv
+            return min([x for x in self.OPEN.values()])
         return -1
 
     def min_state(self):
@@ -77,12 +74,14 @@ class D_star(object):
         # if empty, returns None and -1
         # it also removes this min value form the OPEN set.
         if self.OPEN:
+            mink = -1
             minv = np.inf
-            for v,k in enumerate(self.OPEN):
-                if v < minv: mink, minv = k, v
+            for v, k in enumerate(self.OPEN):
+                if v < minv:
+                    mink, minv = k, v
             return mink, self.OPEN.pop(mink)
         return None, -1
-    
+
     def insert(self, x, h_new):
         # inserting a key and value into OPEN list (x, kx)
         # depending on following situations
@@ -93,31 +92,31 @@ class D_star(object):
         if self.tag[x] == 'Closed':
             kx = min(self.h[x], h_new)
         self.OPEN[x] = kx
-        self.h[x],self.tag[x] = h_new, 'Open'
-            
+        self.h[x], self.tag[x] = h_new, 'Open'
+
     def process_state(self):
         x, kold = self.min_state()
         self.tag[x] = 'Closed'
         self.V.add(x)
         if x == None: return -1
-        if kold < self.h[x]: # raised states
-            for y in children(self,x):
-                a = self.h[y] + cost(self,y,x)
+        if kold < self.h[x]:  # raised states
+            for y in children(self, x):
+                a = self.h[y] + cost(self, y, x)
                 if self.h[y] <= kold and self.h[x] > a:
-                    self.b[x], self.h[x] = y , a
-        if kold == self.h[x]:# lower
-            for y in children(self,x):
-                bb = self.h[x] + cost(self,x,y)
+                    self.b[x], self.h[x] = y, a
+        if kold == self.h[x]:  # lower
+            for y in children(self, x):
+                bb = self.h[x] + cost(self, x, y)
                 if self.tag[y] == 'New' or \
-                    (self.b[y] == x and self.h[y] != bb) or \
-                    (self.b[y] != x and self.h[y] > bb):
+                        (self.b[y] == x and self.h[y] != bb) or \
+                        (self.b[y] != x and self.h[y] > bb):
                     self.b[y] = x
                     self.insert(y, bb)
-        else: 
-            for y in children(self,x):
-                bb = self.h[x] + cost(self,x,y)
+        else:
+            for y in children(self, x):
+                bb = self.h[x] + cost(self, x, y)
                 if self.tag[y] == 'New' or \
-                    (self.b[y] == x and self.h[y] != bb):
+                        (self.b[y] == x and self.h[y] != bb):
                     self.b[y] = x
                     self.insert(y, bb)
                 else:
@@ -125,28 +124,30 @@ class D_star(object):
                         self.insert(x, self.h[x])
                     else:
                         if self.b[y] != x and self.h[y] > bb and \
-                            self.tag[y] == 'Closed' and self.h[y] == kold:
+                                self.tag[y] == 'Closed' and self.h[y] == kold:
                             self.insert(y, self.h[y])
         return self.get_kmin()
 
-    def modify_cost(self,x):
+    def modify_cost(self, x):
         # TODO: implement own function
         # self.c[x][y] = cval
         xparent = self.b[x]
-        if self.tag[x] == 'Closed': self.insert(x , self.h[xparent] + cost(self,x,xparent))
-        # return self.get_kmin()
+        if self.tag[x] == 'Closed':
+            self.insert(x, self.h[xparent] + cost(self, x, xparent))
 
     def modify(self, x):
         self.modify_cost(x)
         while True:
             kmin = self.process_state()
-            if kmin >= self.h[x]: break
+            if kmin >= self.h[x]:
+                break
 
-    def path(self, goal = None):
+    def path(self, goal=None):
         path = []
         if not goal:
             x = self.x0
-        else: x = goal
+        else:
+            x = goal
         start = self.xt
         while x != start:
             path.append([np.array(x), np.array(self.b[x])])
@@ -158,7 +159,7 @@ class D_star(object):
         self.OPEN[self.xt] = 0
         # first run
         while True:
-            #TODO: self.x0 = 
+            # TODO: self.x0 =
             self.process_state()
             visualization(self)
             if self.tag[self.x0] == "Closed":
@@ -167,32 +168,48 @@ class D_star(object):
         self.Path = self.path()
         self.done = True
         visualization(self)
-        plt.pause(0.5)
+        plt.pause(0.2)
         # plt.show()
         # when the environemnt changes over time
-        s = tuple(self.env.start)
-        while s != self.xt:
-            
-            if s == tuple(self.env.start):
-                sparent = self.b[self.x0]
-            else: 
-                sparent = self.b[s]
-            
-            self.env.move_block(a=[0,0,-0.1],s=0.5,block_to_move=1,mode='translation')
-            # self.update_obs()
-            print('updated')
-            if cost(self,s, sparent) == np.inf:
-                self.modify(s)
-            print('modified')
-            self.ind += 1
-        
-        self.Path = self.path()
-        visualization(self)
-        plt.show()
-            
-        
 
-        
+        for i in range(2):
+            self.env.move_block(a=[0, 0, -1], s=0.5, block_to_move=1, mode='translation')
+            visualization(self)
+            plt.pause(0.2)
+
+            s = tuple(self.env.start)
+
+            count = 0
+            count_obs = 0
+            while s != self.xt:
+                count += 1
+                print(count)
+
+                if s == tuple(self.env.start):
+                    sparent = self.b[self.x0]
+                else:
+                    sparent = self.b[s]
+                # self.update_obs()
+
+                if cost(self, s, sparent) == np.inf:
+                    # print(s, "   ", sparent)
+                    count_obs += 1
+                    print(count_obs)
+
+                    self.modify(s)
+                    continue
+
+                self.ind += 1
+                s = sparent
+
+            print("test")
+
+            self.Path = self.path()
+            visualization(self)
+            plt.pause(0.2)
+        plt.show()
+
+
 if __name__ == '__main__':
     D = D_star(1)
     D.run()
